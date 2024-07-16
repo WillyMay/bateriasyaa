fdcdf1abc4708d20bd0026b1e26809c103d9af21
WillyMay <willian.mayer@proton.me>
2024-07-16 16:38:41 -0400

    fixing phone number

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -59,7 +59,7 @@ const Header = () => {
           <button className="btn text-white md:ml-8 font-semibold px-3 py-1 rounded duration-500 md:static ml-0">
             {" "}
             <a
-              href="tel:+56936239484"
+              href="tel:+56967511208"
               className="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-white bg-red-600 rounded-lg border border-white hover:bg-red-900 focus:ring-4 focus:ring-gray-100 dark:text-white dark:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-800"
             >
               <svg

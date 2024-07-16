a15db7bf3cba97ce3f48e0a6c0735b8417b2f9df
WillyMay <willian.mayer@proton.me>
2024-07-16 11:35:23 -0400

    fixing gh-pages

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://willymay.github.op/bateriasyaa",
+  base: "https://willymay.github.io/bateriasyaa",
 })

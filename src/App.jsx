9dfd93e7fefa38936bfe1b63c59bb4d030b94afa
WillyMay <willian.mayer@proton.me>
2024-07-16 11:32:16 -0400

    cards

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import Header from "./components/Header"
 import Hero from "./components/Hero"
+import NewServices from "./components/NewServices"
+import Services from "./components/Services"
 function App() {
 
   return (
     <>
       <Header />
       <Hero />
+      <NewServices />
     </>
   )
 }

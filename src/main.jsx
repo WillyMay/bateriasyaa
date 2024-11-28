2e30b7fee0ce034cade096d2f7807f070d0cdc77
WillyMay <willian.mayer@proton.me>
2024-11-27 21:26:58 -0300

    adding gtm

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import TagManager from 'react-gtm-module'
 
+const tagManagerArgs = {
+  gtmId: "GTM-W94WZJJ7"
+};
+TagManager.initialize(tagManagerArgs);
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
